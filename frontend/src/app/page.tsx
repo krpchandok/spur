b481993472f8789dcafd6b9ef32c1711e875bf83
NathanNCN <ncn.nathanchau.ncn@gmail.com>
2025-06-21 18:13:25 -0400

    Created login page for frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useReadContract, useWriteContract, useAccount } from 'wagmi';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from "./config/web3";
+import { useRouter } from 'next/navigation';
+import { Router } from 'next/router';
 
 export default function Home() {
   const { address, isConnected } = useAccount();
@@ -44,8 +46,15 @@ export default function Home() {
   console.log('Contract data:', data);
   console.log('Contract error:', error);
 
+  const router = useRouter()
+
+  const loginPage = () => {
+    router.push("/Login")
+  }
+
   return (
     <div className="p-8">
+      <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4" onClick={()=>loginPage()} >Login page Test</button>
       <h1 className="text-2xl font-bold mb-4">Education NFT Test</h1>
       
       <div className="space-y-4">
